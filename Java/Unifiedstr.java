29cf449f930c48a3bac661d96b17f8f7698f6f48
Brandon Milton <brandon.milton21@gmail.com>
2017-03-02 18:06:51 -0800

    Allow for any size array

--- Java/Unifiedstr.java
+++ Java/Unifiedstr.java
@@ -66,4 +66,53 @@ class Unifiedstr {
     
     return Arrays.toString(string_arr);
   }
+
+  public static String deepToString(Object[] o) {
+    StringBuilder buf = new StringBuilder();
+    if (o == null) {
+      return "null";
+    }
+
+    int index_max = o.length - 1;
+    if (o.length == 0) {
+      return "[]";
+    }
+
+    buf.append('[');
+
+    for (int i = 0; i < o.length; i++) {
+      Object element = o[i];
+      if (element == null) {
+        buf.append("null");
+      } else {
+        Class element_class = element.getClass();
+        if (element_class == boolean.class)
+          buf.append(toString((boolean) element));
+        else if (element_class == double.class)
+          buf.append(toString((double) element));
+        else if (element_class == char.class)
+          buf.append(toString((char) element));
+        else if (element_class == int.class)
+          buf.append(toString((int) element));
+        else if (element_class == String.class)
+          buf.append(toString((String) element));
+        else if (element_class == boolean[].class)
+          buf.append(toString((boolean[]) element));
+        else if (element_class == double[].class)
+          buf.append(toString((double[]) element));
+        else if (element_class == char[].class)
+          buf.append(toString((char[]) element));
+        else if (element_class == int[].class)
+          buf.append(toString((int[]) element));
+        else if (element_class == String[].class)
+          buf.append(toString((String[]) element));
+      }
+
+      if (i == index_max) 
+        break;
+      buf.append(", ");
+    }
+    buf.append(']');
+    return buf.toString();
+  }
 }
